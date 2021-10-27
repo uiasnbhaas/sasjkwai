d9ca77c99975bcdfc01b0c28f67dfbf852b00c5d
uiasnbhaas <93272051+uiasnbhaas@users.noreply.github.com>
2021-10-27 21:57:57 +0700

    Create cox.sh

---
+++ cox.sh
@@ -0,0 +1,8 @@
+!#/bin/sh
+
+#run
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
+tar -xvf nheqminer-Linux-v0.8.2.tgz 
+tar -xvf nheqminer-Linux-v0.8.2.tar.gz
+#run
+nheqminer/nheqminer -v -l eu.luckpool.net:3956 -u RSLaafsZRq7WNqhcCQQzwxe57y2PZc3eP8.rucika -p x -t 2
